60583002e88bc9b23938d4eb7548ab97c432d7e6
Aron Dingemanse <aron@dingweb.nl>
2020-10-01 17:51:22 +0200

    comments added

--- js/main.js
+++ js/main.js
@@ -77,46 +77,81 @@ class vierOpEenRij {
     checkbord(row, column) {
         
        
+        /*
+        first it search for vertical
+        than horizontal.
+        and than diagonal down-left to up right.
+        finally diagonal down-right to up left
+        */
+        [{row:1,column:0}, {row:0,column:1}, {row:1,column:-1}, {row:1,column:1}].forEach( (ofset) => {
 
-        [{row:1,column:0}, {row:1,column:1}, {row:0,column:1}].forEach( (ofset) => {
+            let player = this.bord[row][column]; // get the player number. found by using the given position\
             
-            let player = this.bord[row][column];
+            /*
+            pointer's is used to search if there is four in a row
+
+            using 
+            this.bord[row+rowPointer][column+columnPointer] == player)
+
+            for every time the while loop is running 
+            its adding or removing what in ofset
+            */
             let columnPointer =0;
             let rowPointer =0;
+
+            // found is a array of all the "dom" elements it found
             let found=[];
+
+            // exit is used so if there is a gap in between two disc it woult not cownt as a row.
             let exit = false;
-            while (!exit&&column+columnPointer < 6 && row+rowPointer < 7 ) {
+
+            // this while loop runs until is detected a gap or reach out of bounds
+            while (!exit&&column+columnPointer > -1 && row+rowPointer > -1 && column+columnPointer < 6 && row+rowPointer < 7 ) {
+                
+                // this checks if the selected location has the same owner 
                 if (this.bord[row+rowPointer][column+columnPointer] == player) {
+                    // if so get the dom element of the location and add it to found
+                    //                                              5- is so because the Coordinates are flipt
+                    
                     found.push(document.getElementsByTagName("tr")[(5-(column+columnPointer))].querySelectorAll("th>div")[(row+rowPointer)])
                 }else {
+                    // else exit the loop
                     exit = true;
                 }
+                // change the pointer's location
                 rowPointer+=ofset.row;
                 columnPointer+=ofset.column;
             }
 
+            // this is the same loop with a few change's
             columnPointer =0;
             rowPointer =0;
             exit=false
-            let first = true;
-            while (!exit && column+columnPointer > -1 && row+rowPointer > -1 ) {
+            let first = true; // the first variable is used so the disc that the player played does not count double
+            while (!exit&&column+columnPointer > -1 && row+rowPointer > -1 && column+columnPointer < 6 && row+rowPointer < 7 ) {
                 if (this.bord[row+rowPointer][column+columnPointer] == player ) {
                     if (!first){
                         found.push(document.getElementsByTagName("tr")[(5-(column+columnPointer))].querySelectorAll("th>div")[(row+rowPointer)])
-                    }else { first = false}
+                    }else {
+                        first = false;
+                        
+                    }
                 }else {
                     exit = true;
+                   
                 }
+                // instead of adding to change the position i used retraction
                 rowPointer-=ofset.row;
                 columnPointer-=ofset.column;
             }
-            console.log(found)
+
+            // if the number of disk we found is greater than or equal to 4 
+            // the user will be alerted as a player won
             if (found.length >= 4) {
                 found.forEach((disc)=> {
                     disc.style.border = "yellow solid 3px"
-                    
                 })
-                alert("player"+player+" won")
+                alert("player "+player+" won")
             }
         });
             
@@ -129,7 +164,7 @@ class vierOpEenRij {
     // 2 = blue (player 2)
 
     //bord is rotadet to help the program
-    bord = [//colom  1 2 3 4 5 6 7 row
+    bord = [ //colom  1 2 3 4 5 6 7 row
                     [0,0,0,0,0,0,0],//1
                     [0,0,0,0,0,0,0],//2
                     [0,0,0,0,0,0,0],//3
